b7aacdaed1dd2c0c63d67ff7f85b58ce106a0f88
wym <wym97sh@163.com>
2021-10-26 17:37:21 -0400

    sleep time

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
 from discord import Webhook, RequestsWebhookAdapter
+import time
 target1 = 'https://api.bscscan.com/api?module=account&action=tokentx&address=0xB20bF6D7f60059dd5dE46F3F0F32665a259ea6C0&page=1&offset=10&startblock=0&endblock=999999999&sort=desc&apikey=NZKNZAZTGAXSWFXWN3APAC9GG387F7TWFK'
 mURL = 'https://discord.com/api/webhooks/902651335550304338/RCiw92QwhmwEq5KzcN6Fy1mWINoIZSvlpbDgnaRWtLBldhlW2p4z19UyzW8lvtYvtYTO'
 webhook = Webhook.from_url(mURL,adapter=RequestsWebhookAdapter())
@@ -16,3 +17,4 @@ while True:
         value = result['value']
         webhook.send('Target: {}\nToken: {}\nDirection: {}\nValue: {}'.format('0xb20bf6d7f60059dd5de46f3f0f32665a259ea6c0',result['tokenName'],direction,value))
         former = blocknum
+        time.sleep(10)
